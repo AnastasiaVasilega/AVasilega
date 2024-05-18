d9d4be990f05c41374d3adc68d5cdcccda3df8f2
AnastasiaVasilega <logika0007@gmail.com>
2024-05-18 16:57:36 +0300

    5_1.cpp

--- 5_1.cpp
+++ 5_1.cpp
@@ -25,20 +25,21 @@ void MaxSum(const vector<int>& v, int& sum, int start, int end) {
     sum = max3[0] + max3[1] + max3[2];
 }
 
-int main() {
+int main() //создает вектор из 10000 случайных чисел от 0 до 99
+{
     const int size = 10000;
     vector<int> v(size);
 
-    generate(v.begin(), v.end(), []() { return rand() % 100; });
+    generate(v.begin(), v.end(), []() { return rand() % 100; }); // генерирует случайное число в диапазоне от 0 до 99 для каждого элемента вектора.
 
     int sum1, sum2;
-    thread t1(MaxSum, ref(v), ref(sum1), 0, size / 2);
+    thread t1(MaxSum, ref(v), ref(sum1), 0, size / 2); //ref(v) - передача вектора v по ссылке для доступа к его содержимому.
     thread t2(MaxSum, ref(v), ref(sum2), size / 2, size);
 
     t1.join();
     t2.join();
 
-    cout << sum1 + sum2 << endl;
+    cout << sum1 + sum2 << endl; // суммы трех максимальных элементов из двух половин вектора v
 
     return 0;
 }
